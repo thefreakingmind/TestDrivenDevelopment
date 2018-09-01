9a1dfec280ff1dae3bc1adad787c117dcf25cb9b
Salman Siddiqui <salmansiddiqui100@gmail.com>
2018-09-01 23:55:36 +0530

    Fixed

--- test.py
+++ test.py
@@ -1,5 +1,18 @@
+import unittest
 from selenium import webdriver
 
-browser = webdriver.Chrome()
-browser.get('http://localhost:5000')
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+
+    def tearDown(self):
+        self.browser.quit()
+        
+        
+    def test_starting_a_new_todo_list(self):
+        self.browser.get('http://localhost:5000')
+        self.assertIn('Django', self.browser.title)
+        
+if __name__ == '__main__':
+    unittest.main()
+
